76028eb6128cba275e1cb77e8591234f78199c75
bumik94 <vaclav.himl@gmail.com>
2024-08-23 15:33:34 +0200

    Added titled border

--- src/main/components/CalendarTable.java
+++ src/main/components/CalendarTable.java
@@ -1,10 +1,8 @@
 package main.components;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.JTableHeader;
-import javax.swing.table.TableColumn;
-import javax.swing.table.TableModel;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.util.Calendar;
@@ -12,25 +10,35 @@ import java.util.Calendar;
 public class CalendarTable extends JPanel {
     private final CalendarModel calendar;
     private final DefaultTableModel tableModel;
-    private final JLabel yearLabel;
-    private final JLabel monthLabel;
+//    private final JLabel yearLabel;
+//    private final JLabel monthLabel;
+    private String borderString;
+    private TitledBorder border;
 
     public CalendarTable() {
         // Set JPanel properties
         this.setLayout(new GridBagLayout());
-        this.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
         GridBagConstraints c = new GridBagConstraints();
 
         // Initialize fields
         this.calendar = new CalendarModel();
-        this.yearLabel = new JLabel(Integer.toString(calendar.getYear()));
-        this.monthLabel = new JLabel(calendar.getFieldName(Calendar.MONTH, Calendar.LONG_STANDALONE));
+        this.borderString = (calendar.getFieldName(Calendar.MONTH, Calendar.LONG_STANDALONE) + " " + calendar.currentYear);
+        this.border = BorderFactory.createTitledBorder(null, borderString, TitledBorder.CENTER, TitledBorder.ABOVE_TOP);
+        this.setBorder(border);
+//        this.yearLabel = new JLabel(Integer.toString(calendar.getYear()));
+//        this.monthLabel = new JLabel(calendar.getFieldName(Calendar.MONTH, Calendar.LONG_STANDALONE));
         this.tableModel = new DefaultTableModel(
                 calendar.getDaysOfMonth(),
                 calendar.getDaysOfWeek(Calendar.SHORT_STANDALONE));
 
         // Set JTable properties
-        JTable calendarTable = new JTable(tableModel);
+        JTable calendarTable = new JTable(tableModel) {
+            // Disable editing cells
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
         calendarTable.setDefaultRenderer(Object.class, new DateRenderer());
         calendarTable.setCellSelectionEnabled(true);
         calendarTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
@@ -45,24 +53,26 @@ public class CalendarTable extends JPanel {
         // Add controls to manipulate calendar
 
         // change calendar fields
-        calendar.setYear(2027);
-        calendar.setMonth(Calendar.FEBRUARY);
+        calendar.setYear(2020);
+        calendar.setMonth(Calendar.JUNE);
         // Update labels
-        yearLabel.setText(Integer.toString(calendar.getYear()));
-        monthLabel.setText(calendar.getFieldName(Calendar.MONTH, Calendar.LONG_STANDALONE));
+//        yearLabel.setText(Integer.toString(calendar.getYear()));
+//        monthLabel.setText(calendar.getFieldName(Calendar.MONTH, Calendar.LONG_STANDALONE));
         // Update table and resize again
         tableModel.setDataVector(calendar.getDaysOfMonth(), calendar.getDaysOfWeek(Calendar.SHORT_STANDALONE));
         tableModel.fireTableDataChanged();
         initCellSizes(calendarTable);
+        borderString = (calendar.getFieldName(Calendar.MONTH, Calendar.LONG_STANDALONE) + " " + calendar.getYear());
+        border.setTitle(borderString);
 
         // Add components to the content pane
         c.gridx = 0;
         c.gridy = 0;
-        this.add(yearLabel, c);
+//        this.add(yearLabel, c);
 
         c.gridx = 0;
         c.gridy = 1;
-        this.add(monthLabel, c);
+//        this.add(monthLabel, c);
 
         c.gridx = 0;
         c.gridy = 2;
